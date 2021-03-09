a3789fbabe3e13fc68629b0413e635f73a978ea9
Jonathan Paez <jfpazto@hotmail.com>
2021-03-09 15:43:43 -0500

    Parte1

---
+++ build/resources/main/static/js/app.js
@@ -0,0 +1,62 @@
+app= (function (){
+    var _funcModify = function (variable) {
+        if(variable != null){
+            var arreglo = variable.map(function(blueprint){
+                return {key:blueprint.name, value:blueprint.points.length}
+            })
+            $("#tabla tbody").empty();
+            arreglo.map(function(blueprint){
+                var temporal = '<tr><td id="nombreActor">'+blueprint.key+'</td><td id="puntos">'+blueprint.value+'</td><td type="button" onclick="app.drawPlan(\''+blueprint.key+'\')">Open</td></tr>';
+                $("#tabla tbody").append(temporal);
+            })
+
+            var valorTotal = arreglo.reduce(function(total, valor){
+                return total.value + valor.value;
+            })
+            document.getElementById("autorLabel").innerHTML = author;
+            document.getElementById("puntosLabel").innerHTML = valorTotal;
+        }
+    };
+
+    var _funcDraw = function (vari) {
+        if (vari) {
+            var lastx = null;
+            var lasty = null;
+            var actx = null;
+            var acty = null;
+            var c = document.getElementById("myCanvas");
+            var ctx = c.getContext("2d");
+
+            ctx.clearRect(0, 0, 500, 500);
+            ctx.beginPath();
+
+            vari.points.map(function (prue){
+                if (lastx == null) {
+                    lastx = prue.x;
+                    lasty = prue.y;
+                } else {
+                    actx = prue.x;
+                    acty = prue.y;
+                    ctx.moveTo(lastx, lasty);
+                    ctx.lineTo(actx, acty);
+                    ctx.stroke();
+                    lastx = actx;
+                    lasty = acty;
+                }
+            });
+        }
+    }
+    return {
+            plansAuthor: function () {
+                author = document.getElementById("autor").value;
+                apimok.getBlueprintsByAuthor(author,_funcModify);
+
+            },
+
+            drawPlan: function(name) {
+                author = document.getElementById("autor").value;
+                obra = name;
+                apimok.getBlueprintsByNameAndAuthor(author,obra,_funcDraw);
+            }
+        };
+})();
